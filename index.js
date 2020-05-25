9885f700170a514358f6d049c98dfae780aec2aa
MikeFranco <mfranco_98@yahoo.com>
2020-05-24 20:35:48 -0500

    added access control methods

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ const port = process.env.PORT || 5001;
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "PUT,GET,POST");
   next();
 });
 
 //Middlewares
+app.use(restify.cors());
 app.use(cors());
 app.use(bodyParser.json());
 
